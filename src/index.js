c676e8b80b1806db6a6f84d1f9cf6ffcbb56c833
kate121296 <katyamacegora1212@gmail.com>
2019-09-17 21:13:28 +0300

    Register

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { SnackbarProvider } from 'notistack';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root=(
+    <SnackbarProvider maxSnack={3}>
+        <App />
+    </SnackbarProvider>
+);
+ReactDOM.render(root , document.getElementById('root'));
 
 serviceWorker.unregister();
