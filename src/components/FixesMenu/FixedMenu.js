37cbe1f7b675d1639ba505923e9b8f2dfb400197
kate121296 <katyamacegora1212@gmail.com>
2019-09-22 21:21:41 +0300

    Dialog

--- src/components/FixesMenu/FixedMenu.js
+++ src/components/FixesMenu/FixedMenu.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {IconButton} from "@material-ui/core";
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
 
 import HomeIcon from "../Icons/HomeIcon";
 import SearchIcon from "../Icons/SearchIcon";
@@ -20,30 +26,61 @@ const useStyles = makeStyles({
     },
     icon:{
         width: '20%',
-
     }
 });
 
 function FixedMenu() {
     const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    function handleClickOpen() {
+        setOpen(true);
+    }
+
+    function handleClose() {
+        setOpen(false);
+    }
     return (
-        <footer className={classes.footerWrapper}>
-            <IconButton className={classes.icon}>
-                <HomeIcon/>
-            </IconButton>
-            <IconButton className={classes.icon}>
-                <SearchIcon/>
-            </IconButton>
-            <IconButton className={classes.icon}>
-                <PlusIcon/>
-            </IconButton>
-            <IconButton className={classes.icon}n>
-                <HeartIcon/>
-            </IconButton>
-            <IconButton className={classes.icon}>
-                <HumanIcon/>
-            </IconButton>
-        </footer>
+        <div>
+            <footer className={classes.footerWrapper}>
+                <IconButton className={classes.icon}>
+                    <HomeIcon/>
+                </IconButton>
+                <IconButton className={classes.icon}>
+                    <SearchIcon/>
+                </IconButton>
+                <IconButton className={classes.icon} onClick={handleClickOpen}>
+                    <PlusIcon/>
+                </IconButton>
+                <IconButton className={classes.icon}n>
+                    <HeartIcon/>
+                </IconButton>
+                <IconButton className={classes.icon}>
+                    <HumanIcon/>
+                </IconButton>
+            </footer>
+            <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+                <DialogTitle id="form-dialog-title">Add new photo to the profile</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        margin="dense"
+                        id="name"
+                        label="Add new src of the photo"
+                        type="text"
+                        fullWidth
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Subscribe
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
     );
 }
 
